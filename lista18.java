82eb59f09448362e7d08cc673f79b96c0143bf5c
alexandrecadan <alexandrecadan@gmail.com>
2017-03-05 13:44:11 -0300

    1ª lista finalizada

---
+++ lista18.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+//programa inverte o nome
+class NomeAoContrario {
+	public static void main(String[] args){
+		
+		//descobrir como pegar o tamanho do array com length
+		String[] nome = new String[100];
+		
+		System.out.println("Digite seu nome completo: ");
+		Scanner scn = new Scanner(System.in);
+		
+		//armazena no array quebrando os nomes com espaço
+		nome = scn.nextLine().split(" ");
+		
+		//percorre o array de trás para frente
+		for (int i = nome.length-1; i >= 0; i--){
+			System.out.println(nome[i]);
+		}
+		
+	}
+}
---
+++ lista19.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+//faz as 4 operações básicas
+class Calculadora {
+	public static void main(String[] args){
+		
+		float arg1 = 0, arg2 = 0, resultado = 0;
+		char operacao;
+
+
+		System.out.println("Primeiro argumento: ");
+		Scanner scnArg1 = new Scanner(System.in);
+		arg1 = scnArg1.nextFloat();
+
+
+		System.out.println("Segundo argumento: ");
+		Scanner scnArg2 = new Scanner(System.in);
+		arg2 = scnArg2.nextFloat();
+
+		do {
+
+		System.out.println("Operação (+, -, / ou X): ");
+		Scanner scnOperacao = new Scanner(System.in);
+		operacao = scnOperacao.next().charAt(0);
+
+		} while (operacao != '+' && operacao != '-' && operacao != '/' && operacao != 'x' && operacao != 'X');
+
+		if (operacao == '+') {
+			resultado = arg1 + arg2;
+		} else if (operacao == '-') {
+			resultado = arg1 - arg2;
+		} else if (operacao == '/') {
+			resultado = arg1 / arg2;
+		} else if (operacao == 'x' || operacao == 'X'){
+			resultado = arg1 * arg2;
+		}
+				
+		System.out.printf("A operação escolhida foi " + operacao + " e o resultado da operação é: %.2f\n", resultado);
+		
+	}
+	
+}
+	
